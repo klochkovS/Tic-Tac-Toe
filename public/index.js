5e0eaf49f560ba3df9aed8f7bcc58e69b5cb6f8e
klochkovS <fluurr@gmail.com>
2018-08-29 21:12:22 +0300

    rewrited in oop style

--- public/index.js
+++ public/index.js
@@ -1,5 +1,3 @@
-const root = document.getElementById('root');
-
 const winCombination = [
   [1, 2, 3],
   [4, 5, 6],
@@ -11,38 +9,104 @@ const winCombination = [
   [3, 5, 7],
 ];
 
-let isCross = true;
+function Game(root) {
+  this._isCross = true;
+  this._isOver = false;
+  this._strokeCount = 0;
+  this._crossStrokes = [];
+  this._circleStrokes = [];
 
-const messageBox = document.createElement('div');
-messageBox.className = 'messageBox';
-messageBox.textContent = isCross ? 'Ходят крестки' : 'Ходят нолики';
-const gameField = document.createElement('div');
-gameField.className = 'gameField';
-root.appendChild(messageBox);
-root.appendChild(gameField);
+  // this._crossList = [];
+  // this._circleList = [];
 
-let strokeCount = 0;
-let crossStrokes = [];
-let circleStrokes = [];
+  this._messageBox = document.createElement('div');
+  this._messageBox.className = 'messageBox';
+  this._messageBox.textContent = this._isCross
+    ? 'Ходят крестки'
+    : 'Ходят нолики';
+  root.appendChild(this._messageBox);
 
-const stopGame = () => {
-  const cellList = document.querySelectorAll('.cell');
-  cellList.forEach((cell) => {
-    cell.removeEventListener('click', stroke);
-  });
+  this._gameField = document.createElement('div');
+  this._gameField.className = 'gameField';
+  root.appendChild(this._gameField);
+
+  this._makeCells(); // Сначала доска потом клетки
+
+  this._restartButton = document.createElement('div');
+  this._restartButton.addEventListener('click', this._newGame.bind(this));
+  this._restartButton.className = 'btn-restart btn-restart_hidden';
+  this._restartButton.textContent = 'Новая игра';
+  root.appendChild(this._restartButton);
+}
+
+Game.prototype._makeCells = function () {
+  for (let i = 0; i < 9; i += 1) {
+    const cell = document.createElement('div');
+    cell.dataset.cellId = i + 1;
+    cell.addEventListener('click', this._stroke.bind(this), { once: true });
+    cell.className = 'cell';
+    this._gameField.appendChild(cell);
+  }
 };
 
-const checkWin = (strokesArr) => {
-  messageBox.textContent = isCross ? 'Ходят крестки' : 'Ходят нолики';
+Game.prototype._newGame = function () {
+  // Нельзя удалить слушатель из-за bind (можно с костылем см.сылку)
+  // поэтому пересоздаю ячейки заново, чтобы не остались старые слушатели событий
+  // https://developer.mozilla.org/ru/docs/Web/API/EventTarget/addEventListener#The_value_of_this_within_the_handler
+  const cellList = document.querySelectorAll('.cell');
+  cellList.forEach((cell) => {
+    cell.remove();
+  });
+  this._makeCells();
+
+  this._crossStrokes = [];
+  this._circleStrokes = [];
+  this._strokeCount = 0;
+  this._isCross = true;
+  this._isOver = false;
+  this._restartButton.className += ' btn-restart_hidden';
+  this._messageBox.textContent = this._isCross
+    ? 'Ходят крестки'
+    : 'Ходят нолики';
+};
+
+Game.prototype._stroke = function ({ target }) {
+  const figure = document.createElement('span');
+
+  if (this._strokeCount % 2) {
+    figure.className = 'circle';
+    this._circleStrokes.push(+target.dataset.cellId);
+    this._checkWin(this._circleStrokes);
+  } else {
+    figure.className = 'cross';
+    this._crossStrokes.push(+target.dataset.cellId);
+    this._checkWin(this._crossStrokes);
+  }
+  this._isCross = !this._isCross;
+  if (!this._isOver) {
+    this._messageBox.textContent = this._isCross
+      ? 'Ходят крестки'
+      : 'Ходят нолики';
+  }
+  target.appendChild(figure);
+  this._strokeCount += 1;
+  if (this._strokeCount === 9 && !this._isOver) {
+    this._messageBox.textContent = 'Ничья!';
+    this._stopGame();
+    this._restartButton.className = 'btn-restart';
+  }
+};
+
+Game.prototype._checkWin = function (strokesArr) {
   winCombination.forEach((winArr) => {
     let count = 0;
     strokesArr.forEach((value) => {
       if (winArr.indexOf(value) !== -1) {
         count += 1;
         if (count === 3) {
-          stopGame();
-          restartButton.className = 'btn-restart';
-          messageBox.textContent = isCross
+          this._stopGame();
+          this._restartButton.className = 'btn-restart';
+          this._messageBox.textContent = this._isCross
             ? 'Победили крестики'
             : 'Победили нолики';
         }
@@ -51,59 +115,12 @@ const checkWin = (strokesArr) => {
   });
 };
 
-
-const stroke = ({ target }) => {
-  const figure = document.createElement('span');
-  target.removeEventListener('click', stroke);
-
-  if (strokeCount % 2) {
-    figure.className = 'circle';
-    circleStrokes.push(+target.dataset.cellId);
-    checkWin(circleStrokes);
-  } else {
-    figure.className = 'cross';
-    crossStrokes.push(+target.dataset.cellId);
-    checkWin(crossStrokes);
-  }
-  isCross = !isCross; // новая игра первый ход проигравшему
-  target.appendChild(figure);
-  strokeCount += 1;
-  if (strokeCount === 9) {
-    messageBox.textContent = 'Ничья!';
-    stopGame();
-    restartButton.className = 'btn-restart';
-  }
+Game.prototype._stopGame = function () {
+  // Тут можно было бы просто сбросить евенты,
+  // но... см. коммент в this._newGame
+  this._isOver = true;
 };
 
 
-const newGame = () => {
-  const crosList = document.querySelectorAll('.cross');
-  const circleList = document.querySelectorAll('.circle');
-  const figureList = [...crosList, ...circleList];
-  figureList.forEach((figure) => {
-    figure.remove();
-  });
-  const cellList = document.querySelectorAll('.cell');
-  cellList.forEach(cell => cell.addEventListener('click', stroke));
-  crossStrokes = [];
-  circleStrokes = [];
-  strokeCount = 0;
-  restartButton.className += ' btn-restart_hidden';
-  messageBox.textContent = isCross ? 'Ходят крестки' : 'Ходят нолики';
-};
-
-
-for (let i = 0; i < 9; i += 1) {
-  const cell = document.createElement('div');
-  cell.dataset.cellId = i + 1;
-  cell.addEventListener('click', stroke);
-
-  cell.className = 'cell';
-  gameField.appendChild(cell);
-}
-
-const restartButton = document.createElement('div');
-restartButton.addEventListener('click', newGame);
-restartButton.className = 'btn-restart btn-restart_hidden';
-restartButton.textContent = 'Новая игра';
-root.appendChild(restartButton);
+const root = document.getElementById('root');
+const game = new Game(root);
